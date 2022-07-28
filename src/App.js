33d480c34ba7e69d08df03f2d7e0dff33abc8508
amit sarraf <amitsarraf80@gmail.com>
2022-07-28 20:34:59 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import './App.css'
 
 function App() {
+
+const [data, setData] = useState([])
+
+  useEffect(()=>{
+     async function fetchData() {
+     const movieData = await fetch("/api");
+      const response = await movieData.json()
+      setData(response)
+    }
+    fetchData();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1 className="heading">Hello from react</h1>
+      </div>
+      <div style={{display: "flex", flexWrap: "wrap" , justifyContent: 'space-between' , marginBottom:"2px"}}>
+        {data.map((item)=>{
+          return(
+            
+        <div className="card" style={{ width: "18rem" }} key={item._id}>,
+          <div className="card-body" >
+            <h5 className="card-title">{item.movieName}</h5>
+            <p className="card-text">
+              {item.rating}
+            </p>
+            <p className="card-text">
+              {item.releasedDate}
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+          )
+})
+}
+      </div>
     </div>
   );
 }
